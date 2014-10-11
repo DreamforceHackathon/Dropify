2923d91abf3e1965354709e3e4a61d116b5569ba
Gary Hammell <garyhammell@Garys-MacBook-Pro.local>
2014-10-11 15:27:07 -0700

    add new and create methods in messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,14 @@
 class MessagesController < ApplicationController
   def index
   end
+
+  def new
+    @message = Message.new
+    render :partial => 'new_message'
+  end
+
+  def create
+    current_user.messages.create(content: params[:content])
+    render nothing: true
+  end
 end
