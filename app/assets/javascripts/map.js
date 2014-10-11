18f97bbc8040adec885c5e354be91b46956522f6
Nicholas Cantelmi <ncantelmi@Nicholass-MacBook-Air-2.local>
2014-10-11 12:42:02 -0700

    Completed geolocation with HTML5.  If HTML5 geolocation doesn't work.  Then it the throws an error message to user.

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -12,6 +12,35 @@ $(document).ready(function() {
     };
     var map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
+
+    // Try HTML5 geolocation
+      if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = new google.maps.LatLng(position.coords.latitude,
+                                           position.coords.longitude);
+
+          var infowindow = new google.maps.InfoWindow({
+            map: map,
+            position: pos,
+            content: 'Cool! I found you.'
+          });
+
+          map.setCenter(pos);
+        }, function() {
+          handleNoGeolocation(true);
+        });
+      } else {
+        // Browser doesn't support Geolocation
+        handleNoGeolocation(false);
+      }
+    }
+
+    function handleNoGeolocation(errorFlag) {
+      if (errorFlag) {
+        var content = 'Error: The Geolocation service failed.';
+      } else {
+        var content = 'Error: Your browser doesn\'t support geolocation.';
+      }
   }
   google.maps.event.addDomListener(window, 'load', initialize);
 });
