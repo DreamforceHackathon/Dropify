4038da27f4c38f29a564696e2266b0bc8113d6a6
yohanyi <yohanyista@gmail.com>
2014-10-11 13:02:57 -0700

    create action for messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,9 +8,18 @@ class MessagesController < ApplicationController
   end
 
   def create
+    new_message = Message.new(content: params[:content], latitude: params[:lat], longitude: params[:long])
+
+    if new_message.save
+      render json: new_message.to_json
+    else
+      render json: {errors: new_message.errors}.to_json
+    end
   end
 
   def show
+    message = Message.find(params[:message_id])
+    
   end
 
   def edit
