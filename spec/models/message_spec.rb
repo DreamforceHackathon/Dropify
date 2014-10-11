170753fa6db8555ae7af4430f256cf84803977da
James Robinson <james.michael.robinson@googlemail.com>
2014-10-11 13:47:11 -0700

    Add message model association tests

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -7,4 +7,10 @@ describe Message do
     it { should validate_presence_of :latitude }
     it { should ensure_length_of(:content).is_at_most(220) }
   end
+
+  context "associations" do
+    it { should belong_to :user }
+    it { should have_many :comments }
+    it { should have_many :votes }
+  end
 end
