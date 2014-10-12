dd8164e52eb125b6f538d6607a1531505a5741f7
Nicholas Cantelmi <ncantelmi@Nicholass-MacBook-Air-2.local>
2014-10-11 22:56:34 -0700

    added test keys for environment variables into "config/initializers/stripe.rb"

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
+  :publishable_key => (ENV['PUBLISHABLE_KEY'] || "pk_test_OXggGkyASwqO57cBS14ufi9K"),
+  :secret_key      => (ENV['SECRET_KEY'] || "sk_test_939Pt6SjQ2WwGdpkoqtNUW1c")
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
