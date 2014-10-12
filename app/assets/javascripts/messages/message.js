2555cdaade2e4436c9705d441d4d7dccc8cd2950
yohanyi <yohanyista@gmail.com>
2014-10-12 03:34:55 -0700

    initial attempt

--- app/assets/javascripts/messages/message.js
+++ app/assets/javascripts/messages/message.js
@@ -2,4 +2,5 @@ Dropify.Message = function(args) {
   this.content = args.content;
   this.latitude = args.latitude;
   this.longitude = args.longitude;
+  this.picturePath = args.picture.picture.url
 }
