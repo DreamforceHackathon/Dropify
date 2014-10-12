37869efbee5929e9b77e7171fbe33f84b9b0b579
James Robinson <james.michael.robinson@googlemail.com>
2014-10-11 21:49:14 -0700

    Fix pin dropping

--- app/assets/javascripts/message_form/messageForm.js
+++ app/assets/javascripts/message_form/messageForm.js
@@ -9,12 +9,12 @@ Dropify.MessageForm.prototype = {
     $(submitSelector).on("click", this.handleSubmitClick.bind(this));
   },
   handleSubmitClick: function(evt) {
+    evt.preventDefault();
     $('#new_message').slideUp();
     $('#drop_message_button').slideDown();
-    this.$contentField.val("")
 
-    evt.preventDefault();
     this.messageHandler.createMessage(this.getContentText());
+    this.$contentField.val("")
   },
   getContentText: function() {
     return this.$contentField.val();
