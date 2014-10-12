369e726fed2c9a328eefda9d3ee185ee25547b44
yohanyi <yohanyista@gmail.com>
2014-10-11 23:10:57 -0700

    fixed merge conflicts

--- app/assets/javascripts/dropify.js
+++ app/assets/javascripts/dropify.js
@@ -1,7 +1,6 @@
 var Dropify = {};
 
 Dropify.App = function() {
-
   var map = new Dropify.Map("#map-canvas");
   var messageHandler = new Dropify.MessageHandler(map);
   var messageForm = new Dropify.MessageForm("#create_message_input", "#create_message_submit", "#create_message_cancel", messageHandler)
