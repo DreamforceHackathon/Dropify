916a26df1502f593d45a411ba68c305bd5879b18
Gary Hammell <garyhammell@Garys-MacBook-Pro.local>
2014-10-12 12:20:53 -0700

    styling for uploading photo

--- app/assets/javascripts/messages/new_photo.js
+++ app/assets/javascripts/messages/new_photo.js
@@ -2,8 +2,17 @@ $(document).ready(function() {
   var bindCreateNewPhoto = function() {
     $('body').on('click', '#create_picture_cancel', function(e){
       e.preventDefault()
-      $('#new_picture').slideUp()
+      $('#new_pic_buttons').slideUp()
       // $('#drop_message_button').slideDown()
+    })
+
+    $('body').on('click', '#new_picture_div', function() {
+      console.log("clicked")
+      $('#picture_field').trigger('click')
+    })
+
+    $('body').on('click', '#new_picture_cancel', function() {
+      $('#create_picture_cancel').trigger('click')
     })
   }
 
