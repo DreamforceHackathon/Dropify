cd3df065adab438bc56b0d5f8baef59f3291552b
Gary Hammell <garyhammell@Garys-MacBook-Pro.local>
2014-10-13 18:24:23 -0700

    remove prior created images from image creation form

--- app/assets/javascripts/messages/new_message.js
+++ app/assets/javascripts/messages/new_message.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-  var bindCreateNewMessage = function() {
+  var bindCreateNewMessage = function() {}
   bindCreateNewMessage()
 })
--- app/assets/javascripts/messages/new_photo.js
+++ app/assets/javascripts/messages/new_photo.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
   var bindCreateNewPhoto = function() {
-    $('body').on('click', '#create_picture_cancel', function(e){
-      e.preventDefault()
+    $('body').on('click', '#new_picture_div', function() {
+      $('#picture_field').trigger('click')
+    })
+
+    $('body').on('click', '#new_picture_cancel', function() {
+      $('#photo_preview_div').slideUp()
       $('#new_pic_buttons').slideUp()
       $.ajax({
         url: '/pictures/clear_all',
@@ -9,20 +13,16 @@ $(document).ready(function() {
       })
     })
 
-    $('body').on('click', '#new_picture_div', function() {
-      $('#picture_field').trigger('click')
-    })
-
-    $('body').on('click', '#new_picture_cancel', function() {
-      $('#create_picture_cancel').trigger('click')
-    })
-
     $('body').on('click', '#new_picture_submit', function() {
       $('#create_picture_submit').trigger('click')
     })
 
+    $('body').on('click', '#new_picture_send', function() {
+      $('#photo_preview_div').slideUp()
+      $('#new_pic_buttons').slideUp()
+    })
+
     $('#new_picture').on('change', function(){
-      $('#new_picture_div h3').html('Photo: ' + $('#picture_field').val())
       $('#new_picture_submit').trigger('click')
     })
   }
